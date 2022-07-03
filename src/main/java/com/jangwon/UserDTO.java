8f4e89af600b0f117f1d89ae3665cf566fd39553
Lamia9304 <lamia9304@naver.com>
2022-07-03 20:43:16 +0900

    user 테스트 코드

--- src/main/java/com/jangwon/UserDTO.java
+++
@@ -1,4 +0,0 @@
-package com.jangwon;
-
-public class UserDTO {
-}
---
+++ src/main/java/com/jangwon/shopping/demo/UserDTO.java
@@ -0,0 +1,18 @@
+package com.jangwon.shopping.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@ToString
+public class UserDTO {
+
+
+
+
+        private String name;
+        private Integer age;
+
+
+}
